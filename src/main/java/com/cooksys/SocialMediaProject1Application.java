5f6c96ecc8bed344f46db1e9ced64d3e0b8af695
Adam Fraser <AJFraser10@GMail.com>
2016-09-19 08:29:15 -0500

    Initial Commit of SocialMediaProject1

---
+++ src/main/java/com/cooksys/SocialMediaProject1Application.java
@@ -0,0 +1,12 @@
+package com.cooksys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaProject1Application.class, args);
+	}
+}
